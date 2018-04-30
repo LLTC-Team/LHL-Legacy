fa2bda3c96976d8c31ef654ee20000d3d7bccc7f
xsun2001 <1563770452@qq.com>
2018-04-30 13:33:33 +0800

    add-apt-repository without confirm

--- ci-script/linux-gcc.sh
+++ ci-script/linux-gcc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y gcc-7 g++-7
