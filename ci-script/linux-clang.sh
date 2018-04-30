1e525014966edf21f2e9b1d05f4eb06d92451fd5
xsun2001 <1563770452@qq.com>
2018-04-30 13:37:28 +0800

    fix

--- ci-script/linux-clang.sh
+++ ci-script/linux-clang.sh
@@ -6,7 +6,7 @@ sudo apt-get install xz-utils build-essential
 
 curl -o clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 tar xf clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-mv clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04 /usr/local/clang_6.0.0
+mv clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-16.04/ /usr/local/clang_6.0.0
 
 export PATH=/usr/local/clang_6.0.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/clang_6.0.0/lib:$LD_LIBRARY_PATH
