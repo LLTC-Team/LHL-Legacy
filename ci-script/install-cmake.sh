cfacb6599a88bc540698165b9951110f78a554f1
xsun2001 <1563770452@qq.com>
2018-10-19 00:11:15 +0800

    upgrade cmake version for linux ci

--- ci-script/install-cmake.sh
+++ ci-script/install-cmake.sh
@@ -3,10 +3,10 @@
 apt-get remove cmake
 apt-get purge --auto-remove cmake
 
-wget https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.sh
-chmod +x ./cmake-3.11.1-Linux-x86_64.sh
+wget https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.sh
+chmod +x ./cmake-3.12.3-Linux-x86_64.sh
 mkdir /opt/cmake
-./cmake-3.11.1-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
+./cmake-3.12.3-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
 ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 cmake -version
