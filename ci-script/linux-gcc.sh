0b69c6e038c4626efb832dc9339276f742cc0b44
xsun2001 <1563770452@qq.com>
2018-10-18 23:47:46 +0800

    add doxygen support

--- ci-script/linux-gcc.sh
+++ ci-script/linux-gcc.sh
@@ -4,5 +4,8 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install -y gcc-7 g++-7
 
+# setup doxygen
+sudo apt-get install -y graphviz doxygen
+
 chmod +x ./install-cmake.sh
 sudo ./install-cmake.sh
