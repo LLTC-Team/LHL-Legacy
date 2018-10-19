2d15bac0cf4d214721e477da92ccefa0024d7a2f
xsun2001 <1563770452@qq.com>
2018-10-19 13:06:05 +0800

    [ci] move path setting to .travis.yml

--- ci-script/install-cmake.sh
+++ ci-script/install-cmake.sh
@@ -4,7 +4,6 @@ sudo apt remove cmake
 sudo apt purge --auto-remove cmake
 
 mkdir $HOME/cmake
-export PATH="$HOME/cmake/bin:$PATH"
 wget https://cmake.org/files/v3.12/cmake-3.12.3-Linux-x86_64.sh
 chmod +x cmake-3.12.3-Linux-x86_64.sh
 ./cmake-3.12.3-Linux-x86_64.sh --prefix=$HOME/cmake --exclude-subdir --skip-license
